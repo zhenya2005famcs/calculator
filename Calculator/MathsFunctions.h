476d056717fb1f73d1024c8358dd0f7ee1d51458
zhenya2005famcs <115953667+zhenya2005famcs@users.noreply.github.com>
2022-12-29 21:09:54 +0300

    Add files via upload

---
+++ Calculator/MathsFunctions.h
@@ -0,0 +1,9 @@
+#include <cmath>
+const double Pi = acos(-1); //ќбъ€вл€ем значение числа ѕи
+double sqrt(double x);
+double Sin(double x);
+double Cos(double x);
+double Ctg(double x);
+double Tan(double x);
+int getRang(char Ch);
+bool Maths(std::stack <Leksema>& Stack_n, std::stack <Leksema>& Stack_o, Leksema& item);
